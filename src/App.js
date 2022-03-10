16b9bdb0c4692b4329dd88b4b38c9431fa8d3d8f
Marko Djuric <markodjuric037@gmail.com>
2022-03-10 22:31:49 +0100

    Todo list with Redux

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Input from './components/Input';
+import './App.css';
+import TodoItem from './components/TodoItem';
+
+import {useSelector} from 'react-redux';
+import {selectTodoList} from './features/todoSlice'
+
+function App() {
+
+  const todoList = useSelector(selectTodoList)
+
+  return (
+    <div className="app">
+      <div className='app__container'>
+        <div className='app__todoContainer'>
+          {
+            todoList.map(item => (
+              <TodoItem 
+                name={item.item} 
+                done={item.done}
+                id={item.id}
+              />
+            ))
+          }
+        </div>
+        <Input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
